a7681c7aafef8720c63cb6a826b9e5141502af99
Kyle Purdon <kyle.purdon@nsidc.org>
2014-12-16 13:29:16 -0700

    clean up web mode

--- dev/kwp-dev-web.el
+++ dev/kwp-dev-web.el
@@ -3,12 +3,22 @@
 ;;; Code:
 
 (add-hook 'web-mode-hook 'run-dev-hook)
+(auto-complete-mode nil)
+(rainbow-delimiters-mode nil)
 
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.sass\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.less\\'" . web-mode))
+
+(setq web-mode-markup-indent-offset 4)
+(setq web-mode-css-indent-offset 4)
+(setq web-mode-code-indent-offset 4)
+(setq web-mode-indent-style 4)
 
 (provide 'kwp-dev-web)
 
