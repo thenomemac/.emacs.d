ca00e27d1f26c602415a97a0a02749335c294e73
Kyle Purdon <kyle.purdon@nsidc.org>
2015-03-03 13:08:42 -0700

    Add basic support for JSX/ReactJs

--- dev/kwp-dev-web.el
+++ dev/kwp-dev-web.el
@@ -3,7 +3,6 @@
 ;;; Code:
 
 (add-hook 'web-mode-hook 'run-dev-hook)
-(auto-complete-mode nil)
 
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
@@ -13,11 +12,17 @@
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.sass\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.less\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 
-(setq web-mode-markup-indent-offset 4)
-(setq web-mode-css-indent-offset 4)
-(setq web-mode-code-indent-offset 4)
-(setq web-mode-indent-style 4)
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
 
 (provide 'kwp-dev-web)
 
