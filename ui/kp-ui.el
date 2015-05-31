ccddb72b3b33f6c71a490ab4641e90df0be46117
kpurdon <kylepurdon@gmail.com>
2015-05-31 13:26:27 -0600

    Remove (re-enable) line wrapping

--- ui/kp-ui.el
+++ ui/kp-ui.el
@@ -10,9 +10,6 @@
 (global-linum-mode t)
 (setq linum-format "%4d \u2502 ")
 
-(setq-default truncate-lines 1)
-(add-hook 'text-mode-hook 'turn-on-visual-line-mode)
-
 (set-default-font "Inconsolata 16")
 
 (require 'neotree)
