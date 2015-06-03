008d8ad1557d6bbcd0896b66fa641a4839f8dbe1
Kyle Purdon <kyle.purdon@nsidc.org>
2015-06-03 08:04:21 -0600

    Add .php to web mode

--- dev/kp-dev-web.el
+++ dev/kp-dev-web.el
@@ -5,6 +5,7 @@
 (require 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
