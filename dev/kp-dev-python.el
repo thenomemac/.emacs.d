tacy/kp-emacs

Enable rainbow delimiters for python and go

dev/kp-dev-python.el
@@ -8,6 +8,10 @@
 (setq py-autopep8-options '("--in-place --pep8-passes 2000 --ignore=E501,W690,W391,W293"))
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
+;; enable rainbow-delimiters
+(require 'rainbow-delimiters)
+(add-hook `go-mode-hook `rainbow-delimiters-mode)
+
 ;; use flycheck not flymake
 (when (require 'flycheck nil t)
   (setq elpy-modules

thenomemac/.emacs.d

dev/kp-dev-python.el
@@ -8,6 +8,10 @@
 (setq py-autopep8-options '("--in-place --pep8-passes 2000 --ignore=E501,W690,W391,W293"))
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
+;; enable rainbow-delimiters
+(require 'rainbow-delimiters)
+(add-hook `python-mode-hook `rainbow-delimiters-mode)
+
 ;; use flycheck not flymake
 (when (require 'flycheck nil t)
   (setq elpy-modules