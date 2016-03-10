c434d5fc50670d7db3d1ebd9335d4ed920c704a5
Kyle Purdon <kp@bit.ly>
2016-03-09 21:28:13 -0700

    Remove unused GOBIN definition

--- development/_golang.el
+++ development/_golang.el
@@ -13,11 +13,6 @@
 (require 'rainbow-delimiters)
 (add-hook `go-mode-hook `rainbow-delimiters-mode)
 
-;; define the bin directory for golang
-(defvar GOBIN (concat (getenv "GOPATH") "/bin"))
-(setq exec-path (cons "/usr/local/go/bin" exec-path))
-(add-to-list 'exec-path GOBIN)
-
 ;; run gofmt on save (really goimports which calls gofmt)
 (setq gofmt-command "goimports")
 (add-hook 'before-save-hook 'gofmt-before-save)
