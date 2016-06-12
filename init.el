f68a1ddfa7a3c86c46d73459e2ebc008c624ac7c
kpurdon <kylepurdon@gmail.com>
2016-06-11 21:47:29 -0600

    Enable smart-mode-line and a few improvements

--- init.el
+++ init.el
@@ -27,5 +27,26 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; enable system clipboard
-(require 'pbcopy)
-(turn-on-pbcopy)
+;; http://emacs.stackexchange.com/questions/10900/copy-text-from-emacs-to-os-x-clipboard
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
+;; enable and configure smart-mode-line
+(setq sml/no-confirm-load-theme t)
+(sml/setup)
+(add-to-list 'sml/replacer-regexp-list '("^/bitly/src/github.com/bitly/bitly" ":bitly:") t)
+
+;; put all backup files in the system temp directory
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
