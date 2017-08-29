26b2b14c3525f4fb783393f7061771cc3eb74b14
Josiah Olson <thenomemac@gmail.com>
2017-08-29 07:58:35 -0400

    with elpa

---
+++ elpa/deferred-20170531.2135/deferred-autoloads.el
@@ -0,0 +1,15 @@
+;;; deferred-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("deferred.el") (22949 22036 469135 849000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; deferred-autoloads.el ends here
